32ccc939f31dd48c195f7e2441df91f22f6568bd
crashdaddy <kennethscoggins@mail.com>
2020-04-19 06:43:22 -0500

    added functionality, css, attributions

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # IsThisLove
 A love tester based on 1 Corinthians 13:4-8
 
+Live Version: https://www.crazyhappyfuntime.com/love/
+
 #Specifications:
 
 4 Love is patient, love is kind. It does not envy, it does not boast, it is not proud. 5 It does not dishonor others, it is not self-seeking, it is not easily angered, it keeps no record of wrongs. 6 Love does not delight in evil but rejoices with the truth. 7 It always protects, always trusts, always hopes, always perseveres.
@@ -14,5 +16,13 @@ UPDATE:
           -- hearts change when user clicks them (empty, half-full, full)
 
           -- that's all
+
+  4/18/20 -- rebuilt the whole thing using flex and vw sizing to scale better to mobile
+
+          -- automated building the HTML
+
+  4/19/20 -- added scoring
+
+             added attributions/support link
 
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 
-
+let criteria = ["Love is Patient",
+"Love is Kind",
+"Love Does Not Envy",
+"Love Does Not Boast",
+"Love is Not Proud",
+"Love Does Not Dishonour Others",
+"Love is Not Self-Seeking",
+"Love is Not Easily Angered",
+"Love Keeps No Record of Wrongs",
+"Love Does Not Delight in Evil",
+"Love Rejoices with the Truth",
+"Love Always Protects",
+"Love Always Trusts",
+"Love Always Hopes",
+"Love Always Perseveres",
+"Love Never Fails"
+]
 
 
 const changeHeart = (elem) => {
+
+    document.getElementById("score").style.display='none';
 
     switch (elem.getAttribute('src')) {
         case 'img/heart_empty.png': {
@@ -20,3 +38,48 @@ const changeHeart = (elem) => {
     }
 
 }
+
+const getScore = () => {
+    let hearts = document.getElementsByClassName("hearts");
+
+    let score = 0;
+    let halfHeart = 3.125;
+    let fullHeart = 6.25;
+
+    for (let i = 0;i<hearts.length;i++) {
+        switch (hearts[i].getAttribute('src')) {
+            case "img/heart_empty.png": {
+                score += 0;
+            }
+            break;
+            case "img/heart_half.png": {
+                score += halfHeart;
+            }
+            break;
+            case "img/heart_full.png": {
+                score += fullHeart;
+            }
+            break;
+        }
+    }
+
+    score/= 2;
+    if (score===100) {score = 0;}
+    document.getElementById("score").innerHTML="Your love value is: " + parseFloat(score).toFixed(2);
+    document.getElementById("score").style.display = 'block';
+}
+
+const setupBoard = () => {
+    let htmlStr = "";
+    for (let i = 0;i<criteria.length;i++) {
+        htmlStr += `<div class="outerDiv">
+        <div style="display:inline;"><img class="hearts" src="img/heart_empty.png" style="width:20vw;" onclick="changeHeart(this)"></div>
+        <div style="display:inline;margin-left: 10px;margin-right: 10px;">${criteria[i]}</div>
+        <div style="display:inline;"><img class="hearts" src="img/heart_empty.png" style="width:20vw;" onclick="changeHeart(this)"></div>
+    </div>`
+    }
+
+    document.getElementById("wrapper").innerHTML = htmlStr;
+}
+
+setupBoard();
